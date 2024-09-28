f43bbed611238de9654f92388ad74fa814cd12a9
HiteshShinde-web <hiteshshinde1094@gmail.com>
2024-09-28 21:18:49 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://real-estate-mern-backend-7e3p.onrender.com',
         secure: false,
       },
     },
